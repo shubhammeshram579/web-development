de334213efa22e364e43147e9da0c9a8fd724cec
shubhammeshram579 <shubhammeshram579@gmail.com>
2023-12-06 02:27:17 +0530

    project working

--- Projects/project 2/script.js
+++ Projects/project 2/script.js
@@ -1,19 +1,43 @@
-gsap.to("#nav",{
-    backroundColor : "#000",
-    height: "120px",
-    duration:0.5,
+
+var crsr = document.querySelector("#cursor")
+var blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove",function(dets){
+  crsr.style.left = dets.x+"px"
+  crsr.style.top = dets.y+"px"
+  blur.style.left = dets.x- 250 +"px"
+  blur.style.top = dets.y - 250 +"px"
+  
+})
+
+
+
+
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "110px",
+    scrollTrigger: {
+      trigger: "#nav",
+      scroller: "body",
+      // markers:true,
+      start: "top -10%",
+      end: "top -11%",
+      scrub: 1,
+    },
+  });
+
+
+
+  gsap.to("#main",{
+    backgroundColor :"#000",
     scrollTrigger:{
-        triggr: "#nav",
-        scroller: "body",
-        markers : true,
-        start : "top -10%",
-        end: "top -11%",
-        scrub:1
+      trigger:"#main",
+      scroller:"body",
+      markers:true,
+      start:"top -25%",
+      end:"top -70%",
+      scrub:1,
     }
-})
-
-
-
-gsap.to("#main",{
-    color: "red"
-})
+  })
