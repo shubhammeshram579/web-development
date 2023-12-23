141ffbfddf7a9991aa159870fa07838cde1e7c0d
shubhammeshram579 <shubhammeshram579@gmail.com>
2023-12-23 10:10:46 +0530

    revision

---
+++ Javascript/JAVASCRIPT/BASIC OF JS/basic of js.js
@@ -0,0 +1,79 @@
+// basic of js print hello wordss
+console.log("hello worlds");
+console.log("shubham meshram");
+
+
+//1. words and keywords in js
+
+// my name is = "shubham"
+// for if in while fuction it is a keywords
+
+
+// 2. varibles in js
+// const, let var tree type of varible in js
+
+var a = "shubham";
+const b = "labham";
+let c = 7038956822;
+
+// update in new varibles
+var aa = a;
+console.log(aa)
+
+const bb = b;
+console.log(bb);
+
+// updating new values 
+// (i) var varible
+a = "meshram";
+console.log(a)
+
+
+
+// (ii) let valribles 
+c = 9903895682;
+console.log(c)
+
+// (iii) const varible
+// b = "sharma";
+// basic of js.js:38  Uncaught TypeError: Assignment to constant variable.
+//     at basic of js.js:38:3
+//  const varible not updated it is fixed do not change values.
+
+// 3 primative data types in js
+
+var str = "shubham";
+var num = 1234567890;
+var nums = 123456n;
+var float = 15.50;
+var int = 1200;
+var buloon = true;
+
+// 4 reprence data types [] () {}
+
+var list = ["shubham","meshram","gondia"];
+var object_list = {"name":[12,22,4,55]};
+var tupples_list = (1,2,34);
+
+// (i) reprence data types 
+// means any copy of value is not copy it is reprence value one varible to another varible storage it's call as refrence values
+// for example
+
+var list2 = [1,3,4,5,6];
+var list3 = list2;
+
+console.log(list3);
+console.log(list2);
+
+// (ii) delete list2 and list3 last values 
+// i can delete list3 to last values it is automatacaly deleted list2 value also
+list3.pop()
+
+console.log(list3);
+console.log(list2);
+
+
+
+
+
+
