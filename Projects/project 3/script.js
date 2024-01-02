e82a640e7722bd322785d8985c2176ed74e81cad
shubhammeshram579 <shubhammeshram579@gmail.com>
2024-01-02 23:25:04 +0530

    done project3 for web development

---
+++ Projects/project 3/script.js
@@ -0,0 +1,154 @@
+
+
+function init(){
+    gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+}
+
+init();
+
+
+var crsr = document.querySelector(".cirsor");
+var main = document.querySelector(".main");
+document.addEventListener("mousemove",function(dets){
+  crsr.style.left = dets.x+ 15 + "px"
+  crsr.style.top = dets.y+ 15 + "px"
+
+});
+
+// var vd = document.querySelector("video");
+// vd.addEventListener("mouseenter",function(dets){
+//   vd.style.left = 
+//   vd.style.top = dets.y+"px"
+// });
+
+var tl = gsap.timeline({
+
+    scrollTrigger:{
+        trigger:".page1 h1",
+        scroller:".main",
+        // markers:true,
+        start:"top 30%",
+        end:"top 0",
+        scrub:3
+    }
+});
+
+
+tl.to(".page1 h1",{
+    x:-100,
+    // duration:1,
+},"amite");
+
+tl.to(".page1 h2",{
+    x:100,
+    // duration:1,
+},"amite");
+
+
+tl.to(".page1 video",{
+    width:"90%"
+},"amite");
+
+
+var tl2 = gsap.timeline({
+
+  scrollTrigger:{
+      trigger:".page1 h1",
+      scroller:".main",
+      // markers:true,
+      start:"top -115%",
+      end:"top -125%",
+      scrub:3
+  }
+});
+
+tl2.to(".main",{
+  backgroundColor: "#fff"
+})
+
+
+var tl3 = gsap.timeline({
+
+  scrollTrigger:{
+      trigger:".page1 h1",
+      scroller:".main",
+      // markers:true,
+      start:"top -460%",
+      end:"top -470%",
+      scrub:3
+  }
+});
+
+tl3.to(".main",{
+  backgroundColor:"#111"
+
+
+})
+
+
+var boxes = document.querySelectorAll(".box");
+
+boxes.forEach(function(elem){
+  elem.addEventListener("mouseenter",function(){
+    var att = elem.getAttribute("data-image");
+    crsr.style.width = "350px"
+    crsr.style.height = "360px"
+    crsr.style.borderRadius = "0"
+    crsr.style.backgroundImage = `url(${att})`
+  })
+
+  elem.addEventListener("mouseleave",function(){
+    elem.style.backgroundColor = "transparent"
+    crsr.style.width = "10px"
+    crsr.style.height = "10px"
+    crsr.style.borderRadius = "50%"
+    crsr.style.backgroundImage = `none`
+  })
+})
+
+
+var h4 = document.querySelectorAll("#nav h4");
+var purple = document.querySelector("#purple");
+
+h4.forEach(function(elem){
+  elem.addEventListener("mouseenter",function(){
+    purple.style.display = "block"
+    purple.style.opacity = "1"
+
+  })
+  elem.addEventListener("mouseleave",function(){
+    purple.style.display = "none"
+    purple.style.opacity = "0"
+
+  })
+})
+
+
+
+
+
+
