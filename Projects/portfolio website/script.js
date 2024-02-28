bcde5df44082dbdefce3c362f20af878204e5a15
shubhammeshram579 <shubhammeshram579@gmail.com>
2024-02-28 11:49:15 +0530

    project

--- Projects/portfolio website/script.js
+++ Projects/portfolio website/script.js
@@ -12,7 +12,7 @@ skillsbtn.addEventListener("click",function(){
     skillssection.style.opacity = "1"
     ExpSection.style.opacity = "0"
     EduSection.style.opacity = "0"
-    skillsbtn.style.borderBottom =  "3px solid yellow"
+    skillsbtn.style.borderBottom =  "3px solid rgb(160, 128, 21)"
     edubtn.style.borderBottom =  "none"
     expbtn.style.borderBottom =  "none"
 
@@ -24,7 +24,7 @@ expbtn.addEventListener("click",function(){
     EduSection.style.opacity = "0"
     skillsbtn.style.borderBottom =  "none"
     edubtn.style.borderBottom =  "none"
-    expbtn.style.borderBottom =  "3px solid yellow"
+    expbtn.style.borderBottom =  "3px solid rgb(160, 128, 21)"
 })
 
 
@@ -34,7 +34,7 @@ edubtn.addEventListener("click",function(){
     ExpSection.style.opacity = "0"
     EduSection.style.opacity = "1"
     skillsbtn.style.borderBottom =  "none"
-    edubtn.style.borderBottom =  "3px solid yellow"
+    edubtn.style.borderBottom =  "3px solid rgb(160, 128, 21)"
     expbtn.style.borderBottom =  "none"
     
 })
@@ -42,5 +42,27 @@ edubtn.addEventListener("click",function(){
 
 
 // project page 
+
+let section = document.querySelectorAll("section");
+let navlink = document.querySelectorAll("nav a");
+
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            navlink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+   
+};
 
 
