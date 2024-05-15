3de60abed64808179be6de488313614541f8b773
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2024-05-15 17:21:29 +0530

    backend mega project update multer file and user cloudinary

---
+++ backend/chai aur backend/chai aur backendP/src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from "cloudinary"
+
+//fs meas file sytems inbuild node js it is helpn to read file delete file and more unliking unlik
+import fs from "fs"
+
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+
+
+// file uplder code method
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader(localFilePath,{
+            resource_type: "auto"
+        })
+        //file has been uplaed sucefull
+        console.log("file is upladed on cloudinary",response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved templory file as the uplad opration got failded
+        return null 
+        
+    }
+}
+
+export {uploadOnCloudinary}
