a71d2d9a047f528bb8c8f46cf2e58b5e4b68d4a1
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2025-12-20 17:29:06 +0530

    updated axios for youtube video

--- youtube/React/FirstVideo/firstreactapp/src/Components2/Pages/Projects.jsx
+++ youtube/React/FirstVideo/firstreactapp/src/Components2/Pages/Projects.jsx
@@ -2,46 +2,50 @@ import React, {useEffect,useState} from "react";
 import axios from "axios";
 
 const Projects = () => {
-  const [product ,setProduct] = useState([])
-
+  const [product,setProduct] = useState([])
 
   useEffect(() => {
 
-    const fatchData = async () => {
-      try {
+  const fatchData = async () => {
+    try {
+      
+      const res = await axios.get(`https://fakestoreapi.com/products`)
 
-       const respose =  await axios.get("https://fakestoreapi.com/products");
-
-      //  console.log(respose);
-
-       setProduct(respose.data)
-        
-      } catch (error) {
-        console.log("somting api error", error)
-      }
-
+      console.log("data",res.data)
+      setProduct(res.data)
+    } catch (error) {
+      console.log("api error",error)
+      
     }
+  }
 
-    fatchData()
-
+  fatchData()
   },[])
- 
 
-  console.log("product",product)
+
+
 
 
   return (
-    <div className="min-h-screen flex items-center justify-center flex-col bg-gray-800">
-      <div className="flex items-center justify-center gap-4  flex-wrap">
-        {product.length > 0 ? 
-        product.map((item) => (
-          <div key={item.id} className="bg-slate-100 px-2 py-2 rounded-lg">
-            <img src={item.image} alt="" className="h-44 w-52 rounded-xl object-cover" />
-            <p className="text-center font-semibold">{item.category}</p>
-          </div> 
-        ))
-        :  <div> proict is not found</div> }
-      </div>
+    <div className="min-h-screen flex items-center justify-center flex-col bg-gray-800 pt-10">
+    {/* <h1 className="text-white">projects</h1> */}
+
+    <div className="grid grid-cols-5 gap-5">
+      {product.length > 0 ?
+      
+      product.map((p,i) => (
+        <div key={p.id} className="bg-white flex items-center justify-center flex-col rounded-xl py-2 px-5">
+          <img src={p.image} alt="" className="h-32 w-42" />
+          <p>name: {p.category}</p>
+          <p>price: {p.price}</p>
+        </div>
+
+      ))
+       
+      : 
+      <div>data not found</div> }
+
+    </div>
     </div>
   );
 };
