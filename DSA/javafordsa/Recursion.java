237b7d350112420b7591490a0205d9361d645448
Shubham Meshram <105120972+shubhammeshram579@users.noreply.github.com>
2026-05-22 01:43:48 +0530

    updated recustion function solve quetion in java

--- DSA/javafordsa/Recursion.java
+++ DSA/javafordsa/Recursion.java
@@ -93,6 +93,55 @@ public class Recursion {
 
     // LEVEL 3 — Intermediate Recursion
 
+    // 5. Fibonacci Series
+
+
+    static int Fibonacci(int n){
+
+        if(n == 0 || n == 1){
+            return n ;
+        }
+
+        return  Fibonacci(n - 1) +  Fibonacci(n - 2);
+
+    }
+
+
+    // 6. Reverse String Using Recursion
+
+    static void reverseStr(String str,int index){
+
+        if(index < 0){
+            return;
+        }
+
+        System.out.print(str.charAt(index));
+        reverseStr(str, index - 1);
+
+    }
+
+
+
+    // 🔵 LEVEL 4 — Important DSA Recursion Problems
+
+    // 7. Check Palindrome Using Recursion
+
+    static boolean isPalindrom(String str, int start, int end){
+
+        if(start >= end){
+            return true;
+        }
+
+        if(str.charAt(start) != str.charAt(end)){
+            return false;
+
+        }
+
+
+        return isPalindrom(str, start + 1, end - 1);
+
+    }
+
 
 
     public static void main(String[] args){
@@ -101,6 +150,17 @@ public class Recursion {
         // printReverse(5);
         // System.out.println(factorialnum(5));
         // System.out.println(sum(5));
+        // System.out.println(Fibonacci(10));
+
+        // String str = "hello";
+
+        // reverseStr(str, str.length() - 1);
+
+        String str = "madam";
+
+        System.out.println(
+            isPalindrom(str, 0, str.length() - 1)
+        );
 
     }
 
